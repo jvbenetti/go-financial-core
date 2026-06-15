9d8a6c93902b76af131027ba70f3d0cc93df5540
JaoVicy <filipimjoa@gmail.com>
2026-06-15 19:19:54 -0300

    feat: creating Status consts

--- internal/model/account.go
+++ internal/model/account.go
@@ -2,11 +2,19 @@ package model
 
 import "time"
 
+type Status string
+
+const (
+	pending Status = "pending"
+	active  Status = "active"
+	blocked Status = "blocked"
+)
+
 type Account struct {
 	ID        string    `json:"id"`
 	UserID    string    `json:"user_id"`
 	Balance   int64     `json:"balance"`  // Balance in cents
 	Currency  string    `json:"currency"` // Ex: "BRL"
-	Status    string    `json:"status"`   // "active", "blocked"
+	Status    Status    `json:"status"`   // "active", "blocked"
 	UpdatedAt time.Time `json:"updated_at"`
 }

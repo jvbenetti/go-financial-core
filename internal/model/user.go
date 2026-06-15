05c39d348393dff60990d2580438b5d5d91c3060
JaoVicy <filipimjoa@gmail.com>
2026-06-14 22:22:34 -0300

    feat: creating User struct

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,1 +1,11 @@
 package model
+
+import "time"
+
+type User struct {
+	ID        string    `json:"id"`
+	Name      string    `json:"name"`
+	Document  string    `json:"document"` // CPF ou CNPJ
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+}

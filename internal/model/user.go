2f3322f30945491099c4fc38b053694edb4da30c
JaoVicy <filipimjoa@gmail.com>
2026-06-16 20:17:45 -0300

    feat: add gorm feats in User struct

--- internal/model/user.go
+++ internal/model/user.go
@@ -3,9 +3,9 @@ package model
 import "time"
 
 type User struct {
-	ID        string    `json:"id"`
-	Name      string    `json:"name"`
-	Document  string    `json:"document"` // CPF ou CNPJ
-	Email     string    `json:"email"`
-	CreatedAt time.Time `json:"created_at"`
+	ID        string    `gorm:"primaryKey;type:uuid" json:"id"`
+	Name      string    `gorm:"type:varchar(255);not null" json:"name"`
+	Document  string    `gorm:"type:varchar(20);uniqueIndex;not null" json:"document"` // Unique CPF or CNPJ
+	Email     string    `gorm:"type:varchar(255);uniqueIndex;not null" json:"email"`
+	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at"`
 }

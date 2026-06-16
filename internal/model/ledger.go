8d99149da2f358725113d6c2f5bcb8a9358f9d82
JaoVicy <filipimjoa@gmail.com>
2026-06-16 20:19:56 -0300

    feat: add gorm feats in Transaction struct and LedgerEntry struct

--- internal/model/ledger.go
+++ internal/model/ledger.go
@@ -4,18 +4,18 @@ import "time"
 
 // The Transaction is macro event (pix to: XYZ)
 type Transaction struct {
-	ID          string    `json:"id"`
-	Description string    `json:"description"`
-	CreatedAt   time.Time `json:"created_at"`
+	ID          string    `gorm:"primaryKey;type:uuid" json:"id"`
+	Description string    `gorm:"type:varchar(255);not null" json:"description"`
+	CreatedAt   time.Time `gorm:"autoCreateTime" json:"created_at"`
 }
 
 // The LedgerEntry is which affected line
 type LedgerEntry struct {
-	ID            string      `json:"id"`
-	TransactionID string      `json:"transaction_id"`
-	Transaction   Transaction `json:"transaction"`
-	AccountID     string      `json:"account_id"`
-	Account       Account     `json:"account"`
-	Amount        int64       `json:"amount"` // Negativo se for Débito, Positivo se for Crédito
-	CreatedAt     time.Time   `json:"created_at"`
+	ID            string      `gorm:"primaryKey;type:uuid" json:"id"`
+	TransactionID string      `gorm:"type:uuid;not null;index" json:"transaction_id"`
+	Transaction   Transaction `gorm:"foreignKey:TransactionID;constraint:OnUpdate:CASCADE,OnDelete:RESTRICT;" json:"transaction"`
+	AccountID     string      `gorm:"type:uuid;not null;index" json:"account_id"`
+	Account       Account     `gorm:"foreignKey:AccountID;constraint:OnUpdate:CASCADE,OnDelete:RESTRICT;" json:"account"`
+	Amount        int64       `gorm:"not null" json:"amount"`
+	CreatedAt     time.Time   `gorm:"autoCreateTime" json:"created_at"`
 }

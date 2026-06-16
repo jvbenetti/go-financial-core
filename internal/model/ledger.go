12432d32fd0c4ea3c7a509d55ac0656a79f8db92
JaoVicy <filipimjoa@gmail.com>
2026-06-16 20:04:55 -0300

    feat: creating LedgerEntry struct

--- internal/model/ledger.go
+++ internal/model/ledger.go
@@ -8,3 +8,12 @@ type Transaction struct {
 	Description string    `json:"description"`
 	CreatedAt   time.Time `json:"created_at"`
 }
+
+// The LedgerEntry is which affected line
+type LedgerEntry struct {
+	ID            string    `json:"id"`
+	TransactionID string    `json:"transaction_id"`
+	AccountID     string    `json:"account_id"`
+	Amount        int64     `json:"amount"` // Negativo se for Débito, Positivo se for Crédito
+	CreatedAt     time.Time `json:"created_at"`
+}

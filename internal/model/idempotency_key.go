2a7a84804cd8e0eeabf61023d769e7a0548f5e1f
JaoVicy <filipimjoa@gmail.com>
2026-06-16 20:21:16 -0300

    feat: add gorm feats in IdempotencyKey struct

--- internal/model/idempotency_key.go
+++ internal/model/idempotency_key.go
@@ -3,8 +3,8 @@ package model
 import "time"
 
 type IdempotencyKey struct {
-	Key          string    `json:"key"`
-	ResponseBody []byte    `json:"response_body"` // Get the first response
-	StatusCode   int       `json:"status_code"`
-	CreatedAt    time.Time `json:"created_at"`
+	Key          string    `gorm:"primaryKey;type:varchar(255)" json:"key"`
+	ResponseBody []byte    `gorm:"type:bytea" json:"response_body"`
+	StatusCode   int       `gorm:"not null" json:"status_code"`
+	CreatedAt    time.Time `gorm:"autoCreateTime" json:"created_at"`
 }

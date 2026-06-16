6e9a2c8df657952bc25ee5c8cffc26d0ca3879b7
JaoVicy <filipimjoa@gmail.com>
2026-06-16 19:47:54 -0300

    feat: cretaing Charge struct

--- internal/model/charge.go
+++ internal/model/charge.go
@@ -1,1 +1,22 @@
 package model
+
+import "time"
+
+type ChargeStatus string
+
+const (
+	pending  ChargeStatus = "pending"
+	paid     ChargeStatus = "paid"
+	expired  ChargeStatus = "expired"
+	refunded ChargeStatus = "refunded"
+)
+
+type Charge struct {
+	ID        string    `json:"id"`
+	AccountID string    `json:"account_id"`  // Conta que vai receber o dinheiro
+	Amount    int64     `json:"amount"`      // Valor em centavos
+	Status    string    `json:"status"`      // "pending", "paid", "expired", "refunded"
+	PixQRCode string    `json:"pix_qr_code"` // Dados do QR Code simulado
+	ExpiresAt time.Time `json:"expires_at"`
+	CreatedAt time.Time `json:"created_at"`
+}

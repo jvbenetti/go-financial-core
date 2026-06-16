5997bec391c9d6506c54c9e1e708b70acc876795
JaoVicy <filipimjoa@gmail.com>
2026-06-16 20:07:20 -0300

    feat: adding new fiels in LedgerEntry struct

--- internal/model/ledger.go
+++ internal/model/ledger.go
@@ -11,9 +11,11 @@ type Transaction struct {
 
 // The LedgerEntry is which affected line
 type LedgerEntry struct {
-	ID            string    `json:"id"`
-	TransactionID string    `json:"transaction_id"`
-	AccountID     string    `json:"account_id"`
-	Amount        int64     `json:"amount"` // Negativo se for Débito, Positivo se for Crédito
-	CreatedAt     time.Time `json:"created_at"`
+	ID            string      `json:"id"`
+	TransactionID string      `json:"transaction_id"`
+	Transaction   Transaction `json:"transaction"`
+	AccountID     string      `json:"account_id"`
+	Account       Account     `json:"account"`
+	Amount        int64       `json:"amount"` // Negativo se for Débito, Positivo se for Crédito
+	CreatedAt     time.Time   `json:"created_at"`
 }

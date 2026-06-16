ead6d220f29d8f900f43e516df5ce803a52ce49d
JaoVicy <filipimjoa@gmail.com>
2026-06-15 21:02:26 -0300

    refactor: change Status to AccountStatus

--- internal/model/account.go
+++ internal/model/account.go
@@ -2,19 +2,19 @@ package model
 
 import "time"
 
-type Status string
+type AccountStatus string
 
 const (
-	pending Status = "pending"
-	active  Status = "active"
-	blocked Status = "blocked"
+	AccountStatusPending AccountStatus = "pending"
+	AccountStatusActive  AccountStatus = "active"
+	AccountStatusBlocked AccountStatus = "blocked"
 )
 
 type Account struct {
-	ID        string    `json:"id"`
-	UserID    string    `json:"user_id"`
-	Balance   int64     `json:"balance"`  // Balance in cents
-	Currency  string    `json:"currency"` // Ex: "BRL"
-	Status    Status    `json:"status"`   // "active", "blocked"
-	UpdatedAt time.Time `json:"updated_at"`
+	ID        string        `json:"id"`
+	UserID    string        `json:"user_id"`
+	Balance   int64         `json:"balance"`  // Balance in cents
+	Currency  string        `json:"currency"` // Ex: "BRL"
+	Status    AccountStatus `json:"status"`   // "active", "blocked"
+	UpdatedAt time.Time     `json:"updated_at"`
 }

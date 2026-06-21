d3497f485f89d7a84329e3140b23c2474823e07f
JaoVicy <filipimjoa@gmail.com>
2026-06-21 17:22:37 -0300

    feat: creating UserRequest struct

--- internal/dto/request/user_req.go
+++ internal/dto/request/user_req.go
@@ -1,1 +1,8 @@
 package request
+
+type UserRequest struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Document string `json:"document"`
+	Phone    string `json:"phone"`
+}

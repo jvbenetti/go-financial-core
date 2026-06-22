b91818b3ba4d1f1b2751505c9c67de1938f35be4
JaoVicy <filipimjoa@gmail.com>
2026-06-22 19:41:09 -0300

    feat: creating Register func

--- internal/handler/user.go
+++ internal/handler/user.go
@@ -1,6 +1,9 @@
 package handler
 
 import (
+	"net/http"
+
+	"github.com/jvbenetti/go-financial-core/internal/dto/request"
 	"github.com/jvbenetti/go-financial-core/internal/service"
 	"github.com/labstack/echo/v5"
 )
@@ -10,5 +13,17 @@ type UserHandler struct {
 }
 
 func (h *UserHandler) Register(c echo.Context) error {
-	
+	req := new(request.UserRequest)
+
+	// 1. Validate JSON
+	if err := c.Bind(req); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid Data!"})
+	}
+
+	resp, err := h.UserService.CreateUserWithAccount(req)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": err.Error()})
+	}
+
+	return c.JSON(http.StatusCreated, resp)
 }

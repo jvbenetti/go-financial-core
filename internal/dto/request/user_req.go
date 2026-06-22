be34e8f5da088f33ef872751aee9cff98db47674
JaoVicy <filipimjoa@gmail.com>
2026-06-22 19:08:27 -0300

    feat: add Password filed in userrequest structs

--- internal/dto/request/user_req.go
+++ internal/dto/request/user_req.go
@@ -2,6 +2,7 @@ package request
 
 type UserRequest struct {
 	Username string `json:"username"`
+	Password string `json:"password"`
 	Email    string `json:"email"`
 	Document string `json:"document"`
 	Phone    string `json:"phone"`

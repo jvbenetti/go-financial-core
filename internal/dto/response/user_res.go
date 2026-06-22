e060dc19aeff6e6e8c5ceca007ab375ea2c2059e
JaoVicy <filipimjoa@gmail.com>
2026-06-22 19:32:49 -0300

    feat: creating UserResponse struct

--- internal/dto/response/user_res.go
+++ internal/dto/response/user_res.go
@@ -1,1 +1,13 @@
 package response
+
+import "github.com/jvbenetti/go-financial-core/internal/model"
+
+type UserResponse struct {
+	ID       uint           `json:"id"`
+	Username string         `json:"username"`
+	Email    string         `json:"email"`
+	Document string         `json:"document"`
+	Phone    string         `json:"phone"`
+	Role     model.UserRole `json:"role"`
+	// Never return the Password
+}

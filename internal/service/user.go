ad0839c73c482212fe15eff321abf201aa4b31aa
JaoVicy <filipimjoa@gmail.com>
2026-06-22 19:36:25 -0300

    feat: starting CreateUserWithAccount func

--- internal/service/user.go
+++ internal/service/user.go
@@ -1,7 +1,15 @@
 package service
 
-import "gorm.io/gorm"
+import (
+	"github.com/jvbenetti/go-financial-core/internal/dto/request"
+	"github.com/jvbenetti/go-financial-core/internal/dto/response"
+	"gorm.io/gorm"
+)
 
 type UserService struct {
 	DB *gorm.DB
+}
+
+func (s *UserService) CreateUserWithAccount(req *request.UserRequest) (*response.UserResponse, error) {
+
 }

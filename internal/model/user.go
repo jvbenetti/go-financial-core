a8b1b4940d0ee5f56cbcfada04b2f50c56bffc39
JaoVicy <filipimjoa@gmail.com>
2026-07-04 16:20:20 -0300

    refactor: change type of phone field

--- internal/model/user.go
+++ internal/model/user.go
@@ -16,7 +16,7 @@ type User struct {
 	Password  string    `gorm:"type:varchar(255);not null" json:"-"`
 	Document  string    `gorm:"type:varchar(20);uniqueIndex;not null" json:"document"` // Unique CPF or CNPJ
 	Email     string    `gorm:"type:varchar(255);uniqueIndex;not null" json:"email"`
-	Phone     int64     `gorm:"type:varchar()"`
+	Phone     string    `gorm:"type:varchar()"`
 	Role      UserRole  `gorm:"default:'customer';type:varchar(50)" json:"role"`
 	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at"`
 }

9d3ec4625278211837b6b936a3e79638d69bc59a
JaoVicy <filipimjoa@gmail.com>
2026-07-04 16:52:27 -0300

    feat: creating tx.Create after defer func()

--- internal/service/user.go
+++ internal/service/user.go
@@ -14,6 +14,7 @@ type UserService struct {
 }
 
 func (s *UserService) CreateUserWithAccount(req *request.UserRequest) (*response.UserResponse, error) {
+	// 1: Creating the user struct
 	user := model.User{
 		Username: req.Username,
 		Email:    req.Email,
@@ -29,11 +30,17 @@ func (s *UserService) CreateUserWithAccount(req *request.UserRequest) (*response
 	// Init transaction
 	tx := s.DB.Begin()
 
-	// If it has error or panic, rollback all!
+	// 2: If it has error or panic, rollback all!
 	defer func() {
 		if r := recover(); r != nil {
 			tx.Rollback()
 		}
 	}()
+
+	// 3: Save the user
+	if err := tx.Create(&user).Error; err != nil {
+		tx.Rollback()
+		return nil, err
+	}
 })
 }

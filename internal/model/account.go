94999aec23033ccdf11128fd9585bf97eafe78b0
JaoVicy <filipimjoa@gmail.com>
2026-07-05 17:24:23 -0300

    feat: creating type CurrencyCode

--- internal/model/account.go
+++ internal/model/account.go
@@ -2,6 +2,14 @@ package model
 
 import "time"
 
+type CurrencyCode string
+
+const (
+	CurrencyBRL CurrencyCode = "BRL"
+	CurrencyUSD CurrencyCode = "USD"
+	CurrencyEUR CurrencyCode = "EUR"
+)
+
 type AccountStatus string
 
 const (
@@ -16,7 +24,7 @@ type Account struct {
 	UserID    string        `gorm:"type:uuid;not null;index" json:"user_id"`
 	User      User          `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:RESTRICT;" json:"user"`
 	Balance   int64         `gorm:"not null;default:0" json:"balance"`
-	Currency  string        `gorm:"type:varchar(3);not null;default:'BRL'" json:"currency"`
+	Currency  CurrencyCode  `gorm:"type:varchar(3);not null;default:'BRL'" json:"currency"`
 	Status    AccountStatus `gorm:"type:varchar(20);not null;default:'pending'" json:"status"`
 	UpdatedAt time.Time     `gorm:"autoUpdateTime" json:"updated_at"`
 }

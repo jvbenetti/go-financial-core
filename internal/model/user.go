390b54e8cdde66aef60622bf1d8c774279907d1c
JaoVicy <filipimjoa@gmail.com>
2026-07-05 17:57:32 -0300

    fix: change type of ID field in user struct

--- internal/model/user.go
+++ internal/model/user.go
@@ -15,7 +15,7 @@ const (
 
 // User is about CPF or CNPJ person
 type User struct {
-	ID        string    `gorm:"primaryKey;type:uuid" json:"id"`
+	ID        uint      `gorm:"primaryKey;type:uuid" json:"id"`
 	Username  string    `gorm:"type:varchar(255);not null" json:"user_name"`
 	Password  string    `gorm:"type:varchar(255);not null" json:"-"`
 	Document  string    `gorm:"type:varchar(20);uniqueIndex;not null" json:"document"` // Unique CPF or CNPJ

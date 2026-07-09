1a93273bba786991f51cc357b6f5d943a884deb0
JaoVicy <filipimjoa@gmail.com>
2026-07-08 21:58:49 -0300

    feat: creating var to db

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,1 +1,14 @@
 package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/jvbenetti/go-financial-core/internal/model"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var DB *gorm.DB

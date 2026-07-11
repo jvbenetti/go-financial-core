ed34bddda94d6e685b69df2527bca8279742467d
JaoVicy <filipimjoa@gmail.com>
2026-07-11 10:07:12 -0300

    feat: calling SetupUserRoutes in RegisterRoutes func

--- internal/route/router.go
+++ internal/route/router.go
@@ -1,7 +1,14 @@
 package route
 
-import "github.com/labstack/echo/v5"
+import (
+	"github.com/jvbenetti/go-financial-core/internal/handler"
+	"github.com/labstack/echo/v5"
+)
 
-func RegisterRoutes(e *echo.Echo) {
+func RegisterRoutes(e *echo.Echo, userHandler *handler.UserHandler) {
 	api := e.Group("/api/v1")
+
+	publicGroup := api.Group("")
+
+	SetupUserRoutes(publicGroup, userHandler)
 }

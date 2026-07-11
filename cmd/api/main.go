f4367b27d96bd2c567dc8fd1a7d3d3f9dc8f1333
JaoVicy <filipimjoa@gmail.com>
2026-07-10 21:59:57 -0300

    style: add comment

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,11 +4,12 @@ import (
 	"log"
 
 	"github.com/jvbenetti/go-financial-core/internal/database"
+	"github.com/jvbenetti/go-financial-core/internal/service"
 )
 
 func main() {
 	// 1. Connect database
-	db, err := database.Connect()
+	db, err := database.Connect() // Only connect
 	if err != nil {
 		log.Fatalf("Error connecting to database: %v", err)
 	}

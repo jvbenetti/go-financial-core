9b73753311ba29cd7fabf34ace35796ab5aa5fff
JaoVicy <filipimjoa@gmail.com>
2026-07-10 21:58:47 -0300

    feat: call connect func

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/jvbenetti/go-financial-core/internal/database"
+)
+
+func main() {
+	// 1. Connect database
+	db, err := database.Connect()
+	if err != nil {
+		log.Fatalf("Error connecting to database: %v", err)
+	}
 }

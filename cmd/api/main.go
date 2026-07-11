f4f7b83c1230db8d3db50e4fda95d584179c38b2
JaoVicy <filipimjoa@gmail.com>
2026-07-11 10:23:54 -0300

    feat: calling the RegisterRoutes func in main

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,7 +5,9 @@ import (
 
 	"github.com/jvbenetti/go-financial-core/internal/database"
 	"github.com/jvbenetti/go-financial-core/internal/handler"
+	"github.com/jvbenetti/go-financial-core/internal/route"
 	"github.com/jvbenetti/go-financial-core/internal/service"
+	"github.com/labstack/echo/v5"
 )
 
 func main() {
@@ -19,5 +21,11 @@ func main() {
 	userService := &service.UserService{DB: db}
 
 	// 3. Call handler
-	userHandler := handler.UserHandler{UserService: userService}
+	userHandler := &handler.UserHandler{UserService: userService}
+
+	// 4. Instances echo
+	e := echo.New()
+
+	// 5. Call Router func
+	route.RegisterRoutes(e, userHandler)
 }

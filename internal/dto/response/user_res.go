c8e35b6dcf69ce38b5fc6bda0fc1f3af85428709
JaoVicy <filipimjoa@gmail.com>
2026-07-14 18:55:10 -0300

    refactor: changing type of ID field in UserResponse struct

--- internal/dto/response/user_res.go
+++ internal/dto/response/user_res.go
@@ -3,7 +3,7 @@ package response
 import "github.com/jvbenetti/go-financial-core/internal/model"
 
 type UserResponse struct {
-	ID       uint           `json:"id"`
+	ID       string         `json:"id"`
 	Username string         `json:"username"`
 	Email    string         `json:"email"`
 	Document string         `json:"document"`
